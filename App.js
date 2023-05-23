046097bb016c7a69ee1943cb6d32ce4d202ff5c3
Lucas Nobre <81994043+lucasnobree@users.noreply.github.com>
2023-05-23 09:27:30 -0300

    slide 11

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
+import React, { useState } from "react"
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import Cabecalho from './src/componentes/cabecalho';
+import NovosItens from './src/componentes/NovosItens';
 
 export default function App() {
+
+  const [lista, setlista] = useState([
+    { texto: "comprar café", key: '1' },
+    { texto: "criar um app", key: '2' },
+    { texto: "jogar video game", key: '3' },
+  ]);
+
+  const apertarItem = (key) => {
+    setlista(
+      (prevLista) => {
+        return prevLista.filter(texto => texto.key != key);
+      }
+    )
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+      <Cabecalho />
+
+      <View style={styles.conteudo}>
+        <View>
+          <FlatList
+            data={lista}
+            renderItem={({ item }) => (
+              <NovosItens props={item} funcao={apertarItem}/>
+            )}
+          />
+        </View>
+      </View >
+    </View >
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#e0e5e5',
   },
+  conteudo: {
+    padding: 40,
+  },
+
 });
